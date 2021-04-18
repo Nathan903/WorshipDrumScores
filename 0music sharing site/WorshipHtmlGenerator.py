370351600d447afe8dcd089a27108197f14ba361
Nathan903 <Nathan903@users.noreply.github.com>
2021-04-18 19:40:19 +0800

    footer fix

--- 0music sharing site/WorshipHtmlGenerator.py
+++ 0music sharing site/WorshipHtmlGenerator.py
@@ -128,8 +128,8 @@ for i in namelist:
 hd=hd+" |</header><br>"
 
 out=hd+out
-out=out+5*"<br>"+"<footer>如音频无法播放代表文件源已阵亡。请到<a href=\"https://github.com/Nathan903/WorshipDrumScores/issues\"style=\"color:red;\">这里</a>提交问题以帮助页面改进</footer>"
-out=out +"  <p>last updated " + str(datetime.datetime.now())+"</p>"
+out=out+5*"<br>"+"<footer>如音频无法播放代表文件源已阵亡。请到<a href=\"https://github.com/Nathan903/WorshipDrumScores/issues\"style=\"color:red;\">这里</a>提交问题以帮助页面改进"
+out=out +"  <br>last updated " + str(datetime.datetime.now())+"</footer>"
 outputfile.write(out+"</html>")
 outputfile.close()
 
