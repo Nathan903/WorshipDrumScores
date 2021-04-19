3583d1efb7b555538a0d6d27c2d28664d134c23b
Nathan903 <Nathan903@users.noreply.github.com>
2021-04-19 23:38:21 +0800

    trim

--- docs/0music sharing site/reduce.py
+++ docs/0music sharing site/reduce.py
@@ -1,6 +1,30 @@
-import htmlmin
-f=open('encrypted.html','r',encoding='utf-8')
-g=open('all.html','w',encoding='utf-8')
-g.write(htmlmin.minify(str(f.read()), remove_empty_space=True).replace("    ","").replace("\n",""))
-g.close()
+import minify_html
+import re
+#https://stackoverflow.com/questions/2319019/using-regex-to-remove-comments-from-source-files
+def removeComments(string):
+    pattern = r"(\".*?\"|\'.*?\')|(/\*.*?\*/|//[^\r\n]*$)"
+    # first group captures quoted strings (double or single)
+    # second group captures comments (//single-line or /* multi-line */)
+    regex = re.compile(pattern, re.MULTILINE|re.DOTALL)
+    def _replacer(match):
+        # if the 2nd group (capturing comments) is not None,
+        # it means we have captured a non-quoted (real) comment string.
+        if match.group(2) is not None:
+            return "" # so we will return empty to remove the comment
+        else: # otherwise, we will return the 1st group
+            return match.group(1) # captured quoted-string
+    return regex.sub(_replacer, string)
+
+src = 'encrypted.html'
+
+f = open(src,'r',encoding='utf-8')
+s=f.read()
+m=removeComments(s)
+o=open('all.html','w',encoding='utf-8')
+
+m = minify_html.minify(m, minify_js=True, minify_css=True)
+o.write(m)
+o.close()
 f.close()
+
+
