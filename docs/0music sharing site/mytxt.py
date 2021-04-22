6b3abfd799d237ff3c268bf567839aaf6007cb4a
Nathan903 <Nathan903@users.noreply.github.com>
2021-04-22 18:52:36 +0800

    favicon update

--- docs/0music sharing site/mytxt.py
+++ docs/0music sharing site/mytxt.py
@@ -23,6 +23,7 @@ def foot():
 <script data-goatcounter="https://meh.goatcounter.com/count"
         async src="//gc.zgo.at/count.js"></script>
 </body>
+<br><img src="logo.png" alt="logo" width="145" height="80"><br>
 <br>
 如音频无法播放代表文件源已阵亡。请到<a href="https://github.com/Nathan903/WorshipDrumScores/issues"style="color:red;">这里</a>提交问题以帮助页面改进
 <br>
