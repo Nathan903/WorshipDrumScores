ab734be93a71a363a2e55997e2df099a69cfc0fb
Nathan903 <Nathan903@users.noreply.github.com>
2021-04-24 11:35:18 +0800

    full width

--- docs/0music sharing site/mytxt.py
+++ docs/0music sharing site/mytxt.py
@@ -32,7 +32,7 @@ last updated """)
 
 
 template= ("""0<p id="$1"><a href="#$1">$2</a></p>
-$3<audio controls loop preload="none">$4<source src="$5" type="audio/mpeg">$6404</audio>
+$3<audio controls loop preload="none" style="width: 100%;">$4<source src="$5" type="audio/mpeg">$6404</audio>
 $7<br>
 """)
 
