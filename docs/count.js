ccca447ecded168d245007405ad7c7e83dfed963
Nathan903 <Nathan903@users.noreply.github.com>
2021-04-27 20:57:14 +0800

    404 custom

--- docs/count.js
+++ docs/count.js
@@ -1,6 +1,4 @@
-// GoatCounter: https://www.goatcounter.com
-// This file (and *only* this file) is released under the ISC license:
-// https://opensource.org/licenses/ISC
+
 ;(function() {
 	'use strict';
 
