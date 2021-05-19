30259fd2861491bff9f0d3f6d7b2fed9b1c1742b
Nathan903 <Nathan903@users.noreply.github.com>
2021-05-19 17:33:46 +0800

    Revert "compress 0514"
    This reverts commit ac864a4d1aa7b5b23960f77249a216379b843771.

--- docs/blog/2021/remove banner.py
+++ docs/blog/2021/remove banner.py
@@ -17,9 +17,9 @@ def read_text_file(file_path):
                 f.write("")
             else:
                 f.write(line)
-date = ""+input("input folder date e.g. 0430: ")
+
 from pathlib import Path
-txt_folder = Path(date).rglob('*index.html')
+txt_folder = Path().rglob('*index.html')
 files = [x for x in txt_folder]
 print(files)
 print("#############")
@@ -27,24 +27,3 @@ print("#############")
 # iterate through all file
 for f in files:
     read_text_file(f)
-print("#")
-def c():
-    def minify(file_path):
-        with open(file_path, 'r', encoding="utf-8") as f:
-            m = minify_html.minify(f.read(), minify_js=True, minify_css=True)
-        with open(file_path, 'w', encoding="utf-8") as f:
-            f.write(m)
-            m=""
-    import minify_html
-    from pathlib import Path
-    txt_folder = Path(date).rglob('*.html')
-    files = [x for x in txt_folder]
-    print(files)
-    print("#############")
-    for f in files:
-        minify(f)
-c()
-
-
-
-        
