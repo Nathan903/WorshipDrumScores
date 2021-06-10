9ad1f93144682a839100e7299031fa439e48aed6
Nathan903 <Nathan903@users.noreply.github.com>
2021-06-10 16:25:53 +0800

    add song

--- docs/0music sharing site/mytxt.py
+++ docs/0music sharing site/mytxt.py
@@ -30,7 +30,7 @@ def head(title,h1="", scorelink=""):
         <a href="/">主页</a>"""
     return ("""
 <!DOCTYPE html>
-<html>
+<html lang="zh">
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
