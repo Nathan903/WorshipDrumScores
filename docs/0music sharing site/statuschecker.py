0c80933abd03df67077dce2458b5ec7ff69f8fe3
Nathan <nathanh4903@gmail.com>
2023-04-04 03:10:11 -0400

    upload

--- docs/0music sharing site/statuschecker.py
+++ docs/0music sharing site/statuschecker.py
@@ -9,11 +9,13 @@ txt=txt.strip().split("\n\n")
 import requests
 for line in txt:
     name, url =line.strip().split("\n")[:2]
+    if url.startswith("/"):
+        url ="https://praise.pages.dev"+url
     if url.startswith("http"):
         try:
             result = requests.get(url).status_code
         except:
-            result = "404"
-            
-        print(result, name,url)
+            result = 404
+        if result ==404:
+            print(result, name,url)
 
